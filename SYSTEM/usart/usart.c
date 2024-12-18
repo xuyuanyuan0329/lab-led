81c92e62c9cd1653e8589fe36fd2966bd0dd22ce
xuyuanyuan0329 <93516469+xuyuanyuan0329@users.noreply.github.com>
2024-12-19 00:57:08 +0300

    Add files via upload

---
+++ SYSTEM/usart/usart.c
@@ -0,0 +1,144 @@
+#include "sys.h"
+#include "usart.h"
+#include "led.h"
+//V1.3修改说明 
+//支持适应不同频率下的串口波特率设置.
+//加入了对printf的支持
+//增加了串口接收命令功能.
+//修正了printf第一个字符丢失的bug
+////////////////////////////////////////////////////////////////////////////////// 	  
+
+
+//加入以下代码,支持printf函数,而不需要选择use MicroLIB
+#if 1
+#pragma import(__use_no_semihosting)             
+//标准库需要的支持函数                 
+struct __FILE 
+{ 
+	int handle; 
+
+}; 
+
+FILE __stdout;       
+//定义_sys_exit()以避免使用半主机模式    
+_sys_exit(int x) 
+{ 
+	x = x; 
+} 
+//重定义fputc函数 
+int fputc(int ch, FILE *f)
+{      
+	while((USART1->SR&0X40)==0);//循环发送,直到发送完毕   
+    USART1->DR = (u8) ch;      
+	return ch;
+}
+#endif 
+
+/*使用microLib的方法*/
+ /* 
+int fputc(int ch, FILE *f)
+{
+	USART_SendData(USART1, (uint8_t) ch);
+
+	while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET) {}	
+   
+    return ch;
+}
+int GetKey (void)  { 
+
+    while (!(USART1->SR & USART_FLAG_RXNE));
+
+    return ((int)(USART1->DR & 0x1FF));
+}
+*/
+ 
+u8 USART_RX_BUF[64];     //接收缓冲,最大64个字节.
+//接收状态
+//bit7，接收完成标志
+//bit6，接收到0x0d
+//bit5~0，接收到的有效字节数目
+u8 USART_RX_STA=0;       //接收状态标记
+u8 Bluetooth_data;
+void uart_init(u32 bound){
+    //GPIO端口设置
+    GPIO_InitTypeDef GPIO_InitStructure;
+	  USART_InitTypeDef USART_InitStructure;
+	  NVIC_InitTypeDef NVIC_InitStructure;
+	 
+	  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1|RCC_APB2Periph_GPIOA|RCC_APB2Periph_AFIO, ENABLE);
+     //USART1_TX   PA.9
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+   
+    //USART1_RX	  PA.10
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);  
+
+   //Usart1 NVIC 配置
+
+    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=3 ;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;		//
+
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
+	NVIC_Init(&NVIC_InitStructure);	//根据NVIC_InitStruct中指定的参数初始化外设NVIC寄存器USART1
+  
+   //USART 初始化设置
+   
+	USART_InitStructure.USART_BaudRate = bound;//一般设置为9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+    USART_Init(USART1, &USART_InitStructure);
+   
+
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//开启中断  接收到数据中断
+   
+    USART_Cmd(USART1, ENABLE);                    //使能串口 
+
+}
+
+void USART1_IRQHandler(void)                	//串口1中断服务程序
+	{
+	    u8 Res;
+	if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)  //接收中断
+		{
+			//USART1_SendByte
+		//	USART_ITConfig
+		USART_ClearITPendingBit(USART1,USART_IT_RXNE);
+		Res =USART_ReceiveData(USART1);//(USART1->DR);	//读取接收到的数据
+	//	USART_SendData(USART1,Res);  
+	//	printf("get data=%d",Res); 
+		Bluetooth_data=Res; 			
+     } 
+	
+		
+} 
+// #if EN_USART1_RX   //如果使能了接收
+// //串口1中断服务程序
+// //注意,读取USARTx->SR能避免莫名其妙的错误   	
+//  
+// u8 BUF_RX_PID;
+// void USART1_IRQHandler(void)
+// {
+// 	u8 res;	
+// #ifdef OS_CRITICAL_METHOD 	//如果OS_CRITICAL_METHOD定义了,说明使用ucosII了.
+// 	OSIntEnter();    
+// #endif
+// 	if(USART1->SR&(1<<5))//接收到数据
+// 	{	 
+// 		res=USART1->DR; 
+// 		BUF_RX_PID=res;
+
+// 	
+// 		
+//   }
+
+
+
---
+++ USER/main.c
@@ -0,0 +1,63 @@
+#include "led.h"
+#include "delay.h"
+#include "sys.h"
+#include "key.h"
+#include "usart.h"
+
+//ALIENTEK Mini STM32开发板范例代码3
+//串口实验
+//技术论坛:www.openedv.com	
+
+ int main(void)
+ {
+	u8 t;
+	u8 len;	
+	u16 times=0;  	
+ 	SystemInit();//系统时钟等初始化
+	delay_init(72);	     //延时初始化
+	NVIC_Configuration();//设置NVIC中断分组2:2位抢占优先级，2位响应优先级
+	uart_init(9600);//串口初始化为9600
+ 	LED_Init();	 //LED端口初始化
+	printf("蓝牙控制LED\r\n"); 
+	 LED0=0;
+	 LED1=0;
+	while(1)
+	{
+		//printf("蓝牙控制LED\r\n");
+	  switch(Bluetooth_data)
+		{
+            case  0x01: LED0=!LED0;
+                        break; 
+						case  0x02: LED1=!LED1;
+												break;
+					  case  0x03: LED0=!LED0;LED1=!LED1;
+					 			        break;
+    }
+	}	 
+
+ }
+ 
+ 					 
+// 		switch(BUF_RX_PID)
+//  		
+// 		{
+
+//  				case 0x02:k=2;	break; //前进标志
+// 				
+//  			    case 0x01: k=1; break;//后退标志
+//  			
+//  				case 0x00: k=0;	break;//停止标志
+//  						
+//  				case 0x03: k=3;	break;//左转标志
+// 				
+// 				case 0x04: k=4;	break;//右转标志
+//  			    
+//  				case 0x05: k=5;	break;//原地高速左转标志
+// 				
+// 				case 0x06: k=6;	break;//原地高速右转标志
+//           
+//              	case 0x07: k=7;	break;//原地低速右转标志
+
+// 				case 0x08: k=8;	break;//原地低速标志
+//  		}
+
